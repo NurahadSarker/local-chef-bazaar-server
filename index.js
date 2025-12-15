4c88a94fdf75e4ae4e52bf600a7bd2a87587d31d
NurahadSarker <nurahadrobi451@gmail.com>
2025-12-15 07:33:45 +0600

    mongodb connect successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -10,10 +11,37 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nurahad.0txpfm4.mongodb.net/?appName=Nurahad`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.get('/', (req, res) =>{
   res.send('Local chef bazaar server is running')
 })
 
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+  }
+
+  finally {
+    
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, ()=>{
   console.log(`local chef bazaar server running on port ${port}`)
